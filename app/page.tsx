9b0fbd11cc85ba92061f8baf22c235f49f974245
Derek Xu <derekxu888@gmail.com>
2025-05-31 16:04:08 -0400

    Add assets (#3)
    * add gradient
    
    * add pr template

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import Link from "next/link";
+import GlowingLogo from "@/public/temp_landing/bhacks-glowing-logo.svg"
 
 export default function Home() {
   return (
@@ -6,6 +8,9 @@ export default function Home() {
       <h1>BostonHacks 2025</h1>
       {/* <Link href={"/apply" + ".html"}>Apply</Link> */}
       {/* <Link href={"/sponsor" + ".html"}>Sponsor Us</Link> */}
+
+      <Image src={GlowingLogo} alt="BostonHacks 2025 Logo" width={300} height={300} />
+      {/* if you need more control over SVG logo, consider using https://react-svgr.com/playground/ */}
 
       <Link href={"/apply"}>Apply</Link>
       <Link href={"/sponsor"}>Sponsor Us</Link>
