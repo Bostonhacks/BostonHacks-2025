771669ab682c7b1777ed8001a2cbd9f5ae48c1c0
Derek Xu <derekxu888@gmail.com>
2025-06-07 19:45:00 -0400

    Update Footer.tsx

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="flex items-center justify-center flex-col text-lg">
+    <div className="flex items-center justify-center flex-col text-sm lg:text-lg">
       <p>Interested in becoming a sponsor?</p>
       <p>Reach us at <a className="underline" href="mailto:contact@bostonhacks.org">contact@bostonhacks.org</a></p>
     </div>
