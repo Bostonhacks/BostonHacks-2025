cbf93fa1e7bdcbd6e496e78e0d8b508c0be0d7da
Derek <derekxu888@gmail.com>
2025-08-01 13:21:32 -0400

    adjust bg color

--- components/Background/Background.tsx
+++ components/Background/Background.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import backgroundImage from "@/public/background.png"
 import waterbead from "@/public/waterbead.svg";
+import cloud1 from "@/public/cloud1.png";
+import cloud2 from "@/public/cloud2.png";
 import Image from "next/image";
 
 type BackgroundProps = {
@@ -9,7 +11,7 @@ type BackgroundProps = {
 
 const Background = ({ children }: BackgroundProps): React.ReactNode => {
   return (
-    <div className="relative w-full bg-gradient-to-b from-[#a1c4fd] to-[#c2e9fb]">
+    <div className={`relative w-full bg-gradient-to-b from-bg-gradient-stop-1 to-bg-gradient-stop-2`}>
       <Image
         className="absolute right-[200px] top-[30%] z-10 opacity-[0.35]"
         src={waterbead}
@@ -17,6 +19,30 @@ const Background = ({ children }: BackgroundProps): React.ReactNode => {
         height={500}
         alt="Waterbead"
       />
+
+      <Image
+        className="absolute right-[200px] top-[10%] z-10 opacity-[0.35]"
+        src={waterbead}
+        width={200}
+        height={200}
+        alt="Waterbead"
+      />
+
+      <Image
+        className="absolute left-0 top-[10%] z-10"
+        src={cloud1}
+        alt="cloud 1"
+        width={1500}
+      />
+
+
+      <Image
+        className="absolute right-0 top-[50%] z-10"
+        src={cloud2}
+        alt="cloud 2"
+        width={2000}
+      />
+
 
       <Image
         className="absolute left-[200px] top-[60%] z-10 opacity-[0.5]"
