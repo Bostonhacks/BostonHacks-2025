ebd530fa6a309f0a54765fa5035388d622ed78c2
Derek Xu <derekxu888@gmail.com>
2025-08-16 09:54:03 -0700

    add font and retroremix (#48)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Background from "@/components/Background";
 import { routes } from "./const";
 import Navbar from "@/components/Navbar";
+import RetroRemix from "./RetroRemix/RetroRemix";
 
 const Home = () => {
   return (
@@ -8,7 +9,9 @@ const Home = () => {
       <Navbar routes={routes} />
       <main className="flex flex-col p-10">
         {/* background gradient defined in Background component */}
-        <div className="flex flex-col items-center justify-center min-h-screen text-white">section</div>
+        <div className="flex flex-col items-center justify-center h-screen text-white">
+          <RetroRemix />
+        </div>
         <div className="flex flex-col items-center justify-center min-h-screen text-white">section</div>
         <div className="flex flex-col items-center justify-center min-h-screen text-white">section</div>
         <div id="about" className="flex flex-col items-center justify-center min-h-screen text-white">section</div>
