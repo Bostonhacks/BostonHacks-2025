9efa41fb11f866d5dfc6a70729f66443e65c4976
Derek Xu <derekxu888@gmail.com>
2025-08-17 14:02:10 -0700

    window fixes (#49)

--- app/const.ts
+++ app/const.ts
@@ -1,4 +1,9 @@
 export const routes = [
   { label: 'About', path: '#about', type: 'hash' as const },
   { label: 'Login', path: '/login', type: 'page' as const },
-]
+];
+
+export const LOGO_SIZE = {
+  width: 50,
+  height: 50,
+};
