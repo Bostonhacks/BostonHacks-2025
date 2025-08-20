babe0ff4cd0e64b96b59eed4422272cac9f064be
alicehan8 <alicehan@bu.edu>
2025-08-19 21:26:53 -0400

    completed title (missing sun ray)

--- app/tracks/title/Title.tsx
+++ app/tracks/title/Title.tsx
@@ -1,18 +1,28 @@
 import Image from "next/image";
 import trackTitle from "@/public/trackTitle.svg";
+import glass from "@/public/glass.svg";
+import cursor from "@/public/cursor.svg";
 
 const TrackTitle = () => {
   return (
     <div className="flex justify-center items-center">
       <div className="relative w-1/2">
         <Image
-          src={trackTitle}
+          src={glass}
           alt="Track Title"
           className="w-full h-auto object-cover pointer-events-none select-none"
         />
-        <span className="absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 text-2xl font-neuropol-x text-white">
+        <span
+          className="absolute inset-0 flex justify-center items-center font-neuropol text-outline-blue"
+          style={{ fontSize: "6vw" }}
+        >
           TRACKS
         </span>
+        <Image
+          src={cursor}
+          alt="Cursor"
+          className="absolute bottom-0 right-0 translate-y-1/2 -translate-x-1/4 w-1/10 h-auto"
+        />
       </div>
     </div>
   );
