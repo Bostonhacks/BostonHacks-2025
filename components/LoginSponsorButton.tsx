4b475de93a80104e26c5269f6e597e772a741289
Caleb Lee <118475347+NotCalebLee@users.noreply.github.com>
2025-08-26 18:35:12 -0400

    Login/Sponsor Us Button (#51)
    * Added Login/Sponsor Us button that routes to /login and /sponsors
    
    * fixed issues with hover location and blur
    
    * adjust images
    
    ---------
    
    Co-authored-by: d0w <derekxu888@gmail.com>

---
+++ components/LoginSponsorButton.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import Link from "next/link"
+import Image from "next/image"  
+import {useState} from "react"
+import LoginButton from "@/public/login_button.png"
+import LoginButtonHover from "@/public/login_button_hover.png"
+import SponsorButton from "@/public/sponsor_button.png"
+import SponsorButtonHover from "@/public/sponsor_button_hover.png"
+
+
+const buttonStyle = "flex items-center rounded-4xl shadow-md shadow-[#345068] hover:drop-shadow-[0_0_30px_rgba(255,255,255,0.4)] cursor-pointer transition-all duration-50";
+
+const LoginSponsorButton = () => {
+    const [loginHover, setLoginHover] = useState(false)
+    const [sponsorHover, setSponsorHover] = useState(false)
+
+    return (
+        <div className="outline-[0.5px] outline-white bg-white/25 backdrop-blur-sm p-4 md:p-4 rounded-2xl flex flex-col gap-4 items-center w-42 sm:w-58 md:w-74 lg:w-[350px] mx-auto">
+
+            {/*Login Button*/}
+            <Link href="/login" //sends to login page
+            > 
+                <button className={buttonStyle}
+                onMouseEnter={() => setLoginHover(true)}
+                onMouseLeave={() => setLoginHover(false)}
+                >
+                    <Image src={loginHover ? LoginButtonHover : LoginButton} //changes image on hover
+                    alt = "Login"
+                    width = {220}
+                    height = {220}
+                    className = "w-32 sm:w-36 md:w-44 lg:w-52 transition-transform duration-200"
+                    ></Image>
+                </button>
+            </Link>
+
+            {/*Sponsors Button*/}
+            <Link href="/sponsors" //sends to sponsors page
+            >
+                <button className={buttonStyle}
+                onMouseEnter={() => setSponsorHover(true)}
+                onMouseLeave={() => setSponsorHover(false)}
+                >
+                    <Image src={sponsorHover ? SponsorButtonHover : SponsorButton} //changes image on hover
+                    alt = "Sponsor Us"
+                    width = {220}
+                    height = {220}
+                    className = "w-32 sm:w-36 md:w-44 lg:w-52 transition-transform duration-200"
+                    ></Image>
+                </button>
+            </Link>
+
+        </div>
+    )
+
+}
+
+
+export default LoginSponsorButton
