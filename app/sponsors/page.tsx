3404d6dbf431eb1ed5adc72c8056143e236992fa
declanyg <declan@youngs.asia>
2025-09-03 13:02:56 -0400

    Finished sponsorship header

---
+++ app/sponsors/page.tsx
@@ -0,0 +1,20 @@
+import Background from "@/components/Background";
+
+import SponsorshipHeader from "@/components/SponsorshipHeader";
+
+const SponsorsPage = () => {
+  return (
+    <Background>
+      <SponsorshipHeader />
+      <main className="flex flex-col">
+        <div className="flex flex-col items-center justify-center min-h-screen text-white">section</div>
+        <div className="flex flex-col items-center justify-center min-h-screen text-white">section</div>
+        <div id="about" className="flex flex-col items-center justify-center min-h-screen text-white">section</div>
+
+        <div className="absolute bottom-0">wapeofijawpfeoiawjfpoawijfawpofijawpoefj</div>
+      </main>
+    </Background>
+  )
+}
+
+export default SponsorsPage;
