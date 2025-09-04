5886cf523591cfd5a23b257a2d338e5f4bcd829a
declanyg <declan@youngs.asia>
2025-09-04 16:12:57 -0400

    Forgot to add sponsorship footer

--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -2,21 +2,26 @@ import Background from "@/components/Background";
 
 import SponsorshipHeader from "@/components/SponsorshipHeader";
 import SponsorshipOpportunities from "@/components/SponsorshipOpportunities";
+import Footer from "@/components/Footer";
 
 const SponsorsPage = () => {
   return (
-    <Background>
-      <SponsorshipHeader />
-      <main className="flex flex-col">
-        <div className="flex flex-col items-center justify-center min-h-screen text-white">
-          <SponsorshipOpportunities />
-        </div>
-        <div className="flex flex-col items-center justify-center min-h-screen text-white">section</div>
-        <div id="about" className="flex flex-col items-center justify-center min-h-screen text-white">section</div>
+    <>
+      <Background>
+        <SponsorshipHeader />
+        <main className="flex flex-col">
+          <div className="flex flex-col items-center justify-center min-h-screen text-white">
+            <SponsorshipOpportunities />
+          </div>
+          <div className="flex flex-col items-center justify-center min-h-screen text-white">section</div>
+          <div id="about" className="flex flex-col items-center justify-center min-h-screen text-white">section</div>
 
-        <div className="absolute bottom-0">wapeofijawpfeoiawjfpoawijfawpofijawpoefj</div>
-      </main>
-    </Background>
+          <div className="absolute bottom-0">wapeofijawpfeoiawjfpoawijfawpofijawpoefj</div>
+        </main>
+      </Background>
+      <Footer/>
+    </>
+    
   )
 }
 
