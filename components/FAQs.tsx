aa536f28edd1ac56a896dd738bdb279e53d8d480
Tiffany Liu <mazingmonkey2004@gmail.com>
2025-09-05 17:26:49 -0400

    using hex instead of variables

--- components/FAQs.tsx
+++ components/FAQs.tsx
@@ -33,7 +33,7 @@ const FaqEntry = ({ question, answer}) => { // each entry will be passed a { que
 
     // each list element in our list of FAQs
     return (
-        <li onClick={handleClick} className="py-5 hover:cursor-pointer group border-b-2 border-[var(--faq-line)]">
+        <li onClick={handleClick} className="py-5 hover:cursor-pointer group border-b-2 border-[#7F90AB]">
             <div className="flex mt-2 justify-between items-center">
                 <div className="question flex w-full items-center gap-x-5">
                     <span className="font-ms-sans-serif text-xl">?.</span>
@@ -95,10 +95,10 @@ const FAQs = () => {
                 className="absolute left-0 -z-10 scale-120 overflow-visible pointer-events-none"
                 priority
             />
-            <div className="bg-[var(--popup-gray)] bg-opacity-20 w-[95%] h-auto border-3 border-t-[white] border-l-[white] border-r-[black] border-b-[black]">
+            <div className="bg-[#BFBFBF] bg-opacity-20 w-[95%] h-auto border-3 border-t-[white] border-l-[white] border-r-[black] border-b-[black]">
                 {/* <p className="flex flex-col items-center justify-center min-h-screen text-blue-500">FAQs???</p> */}
                 <section id="faq" className="w-full flex flex-col justify-center text-text-primary my-[10rem] mt-0">
-                <div className="bg-[var(--popup-blue)] bg-opacity-20 w-[99.5%] h-auto px-5 pt-15 pb-15 -mt-4 ml-3 right-0 ">
+                <div className="bg-[#030087] bg-opacity-20 w-[99.5%] h-auto px-5 pt-15 pb-15 -mt-4 ml-3 right-0 ">
                     <h1 className="font-ms-sans-serif text-white text-7xl px-5">? FAQs</h1>
                 </div>
                 <div className="faqs font-akshar mt-[5rem] w-full grid md:grid-cols-2 grid-cols-1 gap-x-[2rem] z-20 lg:gap-x-[3rem] md:px-[4rem]">
