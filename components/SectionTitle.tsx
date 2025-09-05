96fe24e2e957fcdd59f0b0b0e5fd94de3c9e773e
Declan Young <36024543+declanyg@users.noreply.github.com>
2025-09-05 08:57:41 +0800

    Finished OurSponsors section (#66)
    * Added OurSponsors section

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import glass from "@/public/trackTitle.svg";
 
-const SectionTitle = ({ title }: { title: string }) => {
+const SectionTitle = ({ title, className='text-[6vw]' }: { title: string; className?: string }) => {
   return (
     <div className="flex justify-center items-center">
       <Image
@@ -10,8 +10,7 @@ const SectionTitle = ({ title }: { title: string }) => {
         className="w-full h-auto object-cover pointer-events-none select-none title-shadow"
       />
       <span
-        className="absolute inset-0 flex justify-center items-center font-neuropol text-outline-blue"
-        style={{ fontSize: "6vw" }}
+        className={`absolute inset-0 flex justify-center items-center font-neuropol text-outline-blue ${className}`}
       >
         {title}
       </span>
