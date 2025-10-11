af1465f7077924b928829fc8e397927e5179bb40
Derek Xu <derekxu888@gmail.com>
2025-10-11 11:40:51 -0400

    Add DEVPOST link to constants

--- app/const.ts
+++ app/const.ts
@@ -3,6 +3,7 @@ export const routes = [
   // { label: 'About', path: '#about', type: 'hash' as const },
   // { label: 'Login', path: '/login', type: 'page' as const },
   { label: "DISCORD", path: "https://discord.gg/JsPXpeB7", type: 'page' as const },
+  { label: "DEVPOST", path: "https://bostonhacks-2025.devpost.com", type: 'page' as const },
   { label: "TRACKS", path: "#tracks", type: 'hash' as const },
   { label: "SCHEDULE", path: "#schedule", type: 'hash' as const },
   { label: "FAQs", path: "#faqs", type: 'hash' as const },
